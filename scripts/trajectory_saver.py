eac685c6a400006511a8e07776dfb7ba454728e3
ROBOT_ARM_ROBOCOMP <robot_arm@robocomp.com>
2025-03-11 17:43:42 +0100

    Added support for saving poses

--- scripts/trajectory_saver.py
+++ scripts/trajectory_saver.py
@@ -10,6 +10,10 @@ from trajectory_saver_msg.srv import AddTrajectory, AddTrajectoryResponse
 from trajectory_saver_msg.srv import GetTrajectory, GetTrajectoryResponse
 from trajectory_saver_msg.srv import RemoveTrajectory, RemoveTrajectoryResponse
 
+from trajectory_saver_msg.srv import AddPose, AddPoseResponse
+from trajectory_saver_msg.srv import GetPose, GetPoseResponse
+from trajectory_saver_msg.srv import RemovePose, RemovePoseResponse
+
 from std_srvs.srv import Trigger, TriggerResponse
 
 import rospy
@@ -23,13 +27,39 @@ class TrajectorySaver:
 
         self.node_name_ = node_name
 
-        self.add_trajectory_service = rospy.Service("~/trajectory_saver/add_trajectory", AddTrajectory, self.callback_add_trajectory)
-        self.get_trajectory_service = rospy.Service("~/trajectory_saver/get_trajectory", GetTrajectory, self.callback_get_trajectory)
-        self.remove_trajectory_service = rospy.Service("~/trajectory_saver/remove_trajectory", RemoveTrajectory, self.callback_remove_trajectory)
-        self.amount_trajectory_service = rospy.Service("~/trajectory_saver/amount_of_trajectories", Trigger, self.callback_amount_of_trajectory)
+        self.add_pose_service = rospy.Service("~/robot/arm/trajectory_saver/add_pose", AddTrajectory, self.callback_add_pose)
+        self.add_trajectory_service = rospy.Service("~/robot/arm/trajectory_saver/add_trajectory", AddTrajectory, self.callback_add_trajectory)
+        self.get_pose_service = rospy.Service("~/robot/arm/trajectory_saver/get_pose", GetTrajectory, self.callback_get_pose)
+        self.get_trajectory_service = rospy.Service("~/robot/arm/trajectory_saver/get_trajectory", GetTrajectory, self.callback_get_pose)
+        self.remove_pose_service = rospy.Service("~/robot/arm/trajectory_saver/remove_pose", RemoveTrajectory, self.callback_remove_trajectory)
+        self.remove_trajectory_service = rospy.Service("~/robot/arm/trajectory_saver/remove_trajectory", RemoveTrajectory, self.callback_remove_trajectory)
+        self.amount_trajectory_service = rospy.Service("~/robot/arm/trajectory_saver/amount_of_trajectories", Trigger, self.callback_amount_of_trajectory)
 
         rospy.loginfo("Trajectory Saver Node Started!")
 
+
+    def callback_add_pose(self, req):
+
+        tf_pose = req.name + "_tf"
+        rpy_pose = req.name + "_rpy"
+        joint_pose = req.name + "_joint"
+
+        try:
+            self.msg_store.update_named(tf_pose, req.tf_pose)
+        except:
+                return AddPoseResponse(False, "Failed to add tf pose")
+
+        try:
+            self.msg_store.update_named(rpy_pose, req.rpy_pose)
+        except:
+                return AddPoseResponse(False, "Failed to add rpy pose")
+
+        try:
+            self.msg_store.update_named(joint_pose, req.joint_pose)
+        except:
+                return AddPoseResponse(False, "Failed to add joint pose")
+
+        return AddPoseResponse(True, "Updated pose")
 
     def callback_add_trajectory(self, req):
 
@@ -52,6 +82,37 @@ class TrajectorySaver:
 
         return AddTrajectoryResponse(True, "Updated")
 
+    def callback_get_pose(self, req):
+
+        tf_pose = req.name + "_tf"
+        rpy_pose = req.name + "_rpy"
+        joint_pose = req.name + "_joint"
+
+        try:
+            tf_pose_msg = self.msg_store.query_named(tf_pose, String._type)
+        except:
+            return GetPoseResponse(False, "Database connection failed")
+
+        if(tf_pose_msg[0] is None):
+            return GetPoseResponse(False, "TF Pose does not exist")
+
+        try:
+            rpy_pose_msg = self.msg_store.query_named(rpy_pose, String._type)
+        except:
+            return GetPoseResponse(False, "Database connection failed")
+
+        if(rpy_pose_msg[0] is None):
+            return GetPoseResponse(False, "RPY Pose does not exist")
+
+        try:
+            joint_pose_msg = self.msg_store.query_named(joint_pose, String._type)
+        except:
+            return GetPoseResponse(False, "Database connection failed")
+
+        if(joint_pose_msg[0] is None):
+            return GetPoseResponse(False, "JOINT Pose does not exist")
+
+        return GetPoseResponse(True, "", tf_pose_msg, rpy_pose_msg, joint_pose_msg)
 
     def callback_get_trajectory(self, req):
 
@@ -64,6 +125,59 @@ class TrajectorySaver:
             return GetTrajectoryResponse(False, "NOT EXISTS")
 
         return GetTrajectoryResponse(True, str(trajectory_msg[0].data))
+
+    def callback_remove_pose(self, req):
+
+        tf_pose = req.name + "_tf"
+        rpy_pose = req.name + "_rpy"
+        joint_pose = req.name + "_joint"
+
+        try:
+            tf_pose_msg = self.msg_store.query_named(tf_pose, String._type)
+        except:
+            return RemovePoseResponse(False, "Database connection failed")
+
+        if(tf_pose_msg[0] is None):
+            return RemovePoseResponse(False, "Could not find tf pose in database")
+
+        tf_pose_msg_id = tf_pose_msg[1]["_id"]
+
+        try:
+            self.msg_store.delete(str(tf_pose_msg_id))
+        except:
+            return RemovePoseResponse(False, 'Failed to delete tf pose from database')
+
+        try:
+            rpy_pose_msg = self.msg_store.query_named(rpy_pose, String._type)
+        except:
+            return RemovePoseResponse(False, "Database connection failed")
+
+        if(rpy_pose_msg[0] is None):
+            return RemovePoseResponse(False, "Could not find rpy pose in database")
+
+        rpy_pose_msg_id = rpy_pose_msg[1]["_id"]
+
+        try:
+            self.msg_store.delete(str(rpy_pose_msg_id))
+        except:
+            return RemovePoseResponse(False, 'Failed to delete rpy pose from database')
+
+        try:
+            joint_pose_msg = self.msg_store.query_named(joint_pose, String._type)
+        except:
+            return RemovePoseResponse(False, "Database connection failed")
+
+        if(joint_pose_msg[0] is None):
+            return RemovePoseResponse(False, "Could not find joint pose in database")
+
+        joint_pose_msg_id = joint_pose_msg[1]["_id"]
+
+        try:
+            self.msg_store.delete(str(joint_pose_msg_id))
+        except:
+            return RemovePoseResponse(False, 'Failed to delete joint pose from database')
+
+        return RemovePoseResponse(True, '')
 
     def callback_remove_trajectory(self, req):
 
